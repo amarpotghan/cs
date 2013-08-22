6cebf24eee0480dc44c4a03876a129d85e24916a
amar <amar@inbetween.co.in>
2013-08-22 16:16:04 +0530

    Base URL introduced and the hardcoded urls removed from all app except in css

--- pub-controller/src/main/webapp/WEB-INF/pages/engine/inMemory/EngineDataStore.js
+++ pub-controller/src/main/webapp/WEB-INF/pages/engine/inMemory/EngineDataStore.js
@@ -1,7 +1,17 @@
 var EngineDataStore = function(){
     var screenMappingObject;
     var apiMappingObject;
+    var baseURL;
 }
+
+EngineDataStore.setBaseURL = function(url){
+    this.baseURL = url;
+}
+
+EngineDataStore.getBaseURL = function(){
+    return this.baseURL;
+}
+
 
 EngineDataStore.setScreenMappingObject = function(obj){
     this.screenMappingObject = obj;
