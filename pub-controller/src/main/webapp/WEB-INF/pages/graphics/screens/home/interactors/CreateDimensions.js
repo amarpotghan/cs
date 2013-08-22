53010dd6516f41323e473b41c251584f93b2d122
amar <amar@inbetween.co.in>
2013-08-22 16:45:26 +0530

    Merged the code for Create Dim services.

--- pub-controller/src/main/webapp/WEB-INF/pages/graphics/screens/home/interactors/CreateDimensions.js
+++ pub-controller/src/main/webapp/WEB-INF/pages/graphics/screens/home/interactors/CreateDimensions.js
@@ -2,11 +2,7 @@ function CreateDimensions(){
 
 }
 
-<<<<<<< HEAD:pub-controller/src/main/webapp/WEB-INF/graphics/screens/home/services/Dimensions.js
-Dimensions.createDim = function(prefix,action,name,currentPath,flag,callBack){
-=======
-CreateDimensions.createDim = function(prefix,action,name,currentPath,flag,callMe){
->>>>>>> 242fa02ac23fd67896cee766432519ad5daae254:pub-controller/src/main/webapp/WEB-INF/pages/graphics/screens/home/interactors/CreateDimensions.js
+CreateDimensions.createDim = function(prefix,action,name,currentPath,flag,callBack){
     Router.forward(prefix+action+"/name/"+name+"/path/"+currentPath+"/folder/"+flag,true,function(data){
         callBack(data);
     });
