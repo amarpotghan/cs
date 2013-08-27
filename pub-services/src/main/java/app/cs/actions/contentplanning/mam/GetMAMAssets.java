bb33265b5d23d7720c13bfe53ed48282842b4131
amarpotghan <amarpotghan@gmail.com>
2013-08-27 21:34:21 +0530

    resolving bean exception

--- pub-services/src/main/java/app/cs/actions/contentplanning/mam/GetMAMAssets.java
+++ pub-services/src/main/java/app/cs/actions/contentplanning/mam/GetMAMAssets.java
@@ -3,17 +3,17 @@ package app.cs.actions.contentplanning.mam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import app.cs.interfaces.assets.AssetsRepository;
+import app.cs.impl.mam.MAMRepository;
 import app.cs.model.response.ResponseModel;
 import app.cs.model.response.StringResponse;
 
 @Component
 public class GetMAMAssets {
 
-	private AssetsRepository mamRepository;
+	private MAMRepository mamRepository;
 
 	@Autowired
-	public GetMAMAssets(AssetsRepository mamRepository) {
+	public GetMAMAssets(MAMRepository mamRepository) {
 		this.mamRepository = mamRepository;
 	}
 
--- pub-services/src/main/java/app/cs/actions/contentplanning/mam/MAMInteractions.java
+++ pub-services/src/main/java/app/cs/actions/contentplanning/mam/MAMInteractions.java
@@ -1,17 +1,19 @@
 package app.cs.actions.contentplanning.mam;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import app.cs.impl.mam.MAMRepository;
 import app.cs.interfaces.assets.AssetsRepository;
 
 @Component
 public class MAMInteractions {
-	
+
 	private AssetsRepository mamRepository;
-	
+
 	@Autowired
-	public MAMInteractions(AssetsRepository mamRepository) {
+	public MAMInteractions(MAMRepository mamRepository) {
 		this.mamRepository = mamRepository;
 	}
 
