e64c9026b0471aefa3af7891f44634484e3626eb
amar <amar@inbetween.co.in>
2013-08-27 22:23:33 +0530

    Json for assetchildtree json changed as required

--- pub-controller/src/main/webapp/WEB-INF/pages/graphics/screens/home/scripts/inHouseScripts/js/DynaLazyTree.js
+++ pub-controller/src/main/webapp/WEB-INF/pages/graphics/screens/home/scripts/inHouseScripts/js/DynaLazyTree.js
@@ -7,7 +7,12 @@ var DynaLazyTree = function(){
                 },
                 onLazyRead: function(node){
                     node.appendAjax({
-                        url: "/pub-controller/testdrive/mocks/tree/AssetChildTree.json"
+                        url: "/pub-controller/testdrive/mocks/tree/AssetChildTree.json",
+                        success: function(node, data) {
+                            alert(JSON.stringify(data))
+                            // Called after nodes have been created and the waiting icon was removed.
+                            // 'this' is the options for this Ajax request
+                        }
                     });
                 }
             });
