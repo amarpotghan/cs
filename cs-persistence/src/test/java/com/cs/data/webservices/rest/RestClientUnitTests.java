2703b7e7cdd9add2b1a9415306f909d2fe240149
Pankaj <Pankajgajjar@gmail.com>
2013-09-02 14:22:58 +0530

    move header parameters preparation functionality to method
    prepareHeaderParameters() method impl

--- cs-persistence/src/test/java/com/cs/data/webservices/rest/RestClientUnitTests.java
+++ cs-persistence/src/test/java/com/cs/data/webservices/rest/RestClientUnitTests.java
@@ -1,15 +1,11 @@
 package com.cs.data.webservices.rest;
 
+import static org.fest.assertions.Assertions.assertThat;
+
 import java.util.HashMap;
 import java.util.Map;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
-
-import static org.fest.assertions.Assertions.*;
-import static org.mockito.Mockito.*;
 
 public class RestClientUnitTests {
 
@@ -23,6 +19,18 @@ public class RestClientUnitTests {
 		Map<String, String> headerParameters = new HashMap<String, String>();
 		String result = "result";
 
+		prepareHeaderParameters(headerParameters);
+		restClient = new RestClient();
+		// when
+		String products = restClient.get(url, headerParameters);
+
+		// then
+		assertThat(products).isNotNull();
+		assertThat(products).contains("62");
+
+	}
+
+	private void prepareHeaderParameters(Map<String, String> headerParameters) {
 		headerParameters.put("Accept-Language", "en-US,en;q=0.8");
 		headerParameters.put("Host", "192.168.135.108");
 		headerParameters
@@ -32,14 +40,6 @@ public class RestClientUnitTests {
 		headerParameters.put("Accept", "*/*");
 		headerParameters
 				.put("Accept-Charset", "ISO-8859-1,utf-8;q=0.7,*;q=0.3");
-		restClient = new RestClient();
-		// when
-		String products = restClient.get(url, headerParameters);
-
-		// then
-		assertThat(products).isNotNull();
-		assertThat(products).contains("62");
-
 	}
 
 }
