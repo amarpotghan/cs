2514a73c449f9259f6a090d207860d07d334fa44
amarpotghan <amarpotghan@gmail.com>
2013-09-03 16:29:12 +0530

    removing pub -controller

--- pub-controller/src/main/webapp/WEB-INF/graphics/screens/home/services/Dimensions.js
+++
@@ -1,9 +0,0 @@
-function Dimensions(){
-
-}
-
-Dimensions.createDim = function(prefix,action,name,currentPath,flag,callMe){
-    Router.forward(prefix+action+"/name/"+name+"/path/"+currentPath+"/folder/"+flag,true,function(data){
-        callMe(data);
-    });
-}
