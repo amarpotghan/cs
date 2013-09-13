46e5765a7ccd90aa643692be929199e1a61858b7
amarpotghan <amarpotghan@gmail.com>
2013-09-13 13:37:35 +0530

    adding dell server CS URL

--- cs-repository/src/main/java/app/cs/impl/mam/MAMRepository.java
+++ cs-repository/src/main/java/app/cs/impl/mam/MAMRepository.java
@@ -20,11 +20,11 @@ public class MAMRepository implements AssetsRepository {
 	private static final String X_REQUESTED_WITH = "X-Requested-With";
 	private static final String USER_AGENT_INFO = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.63 Safari/537.31";
 	private static final String USER_AGENT = "User-Agent";
-	private static final String HOSTIP = "192.168.135.108";
+	private static final String HOSTIP = "192.168.135.104";
 	private static final String HOST = "Host";
 	private static final String LANGUAGE = "en-US,en;q=0.8";
 	private static final String ACCEPT_LANGUAGE = "Accept-Language";
-	private final String BASE_URL = "http://192.168.135.108/CS13.0/admin";
+	private final String BASE_URL = "http://192.168.135.104/CS13.0Trunk/admin";
 	private final String LIST_URL = BASE_URL + "/rest/mam/list/";
 	private final String SEARCH_URL = BASE_URL + "/rest/mam/search/";
 	private IRestClient client;
