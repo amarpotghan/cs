48fd449b4c6bea7c6c5531ef274f6b29b3f1ebf4
amarpotghan <amarpotghan@gmail.com>
2013-09-17 22:34:11 +0530

    adding some bug fixes

--- delivery/src/main/webapp/WEB-INF/pages/graphics/screens/home/interactors/CreateDimensions.js
+++ delivery/src/main/webapp/WEB-INF/pages/graphics/screens/home/interactors/CreateDimensions.js
@@ -3,7 +3,16 @@ function CreateDimensions(){
 }
 
 CreateDimensions.createDim = function(prefix,action,input,currentPath,flag,callBack){
-    Router.forwardWithPost(prefix+action+"/name/"+input.name+"/path/"+currentPath+"/folder/"+flag,true,input,function(data){
+	var reqBody = new Object();
+     reqBody.name=input.name;
+     reqBody.managerName=input.managerName;
+     reqBody.startDate=input.startDate;
+     reqBody.endDate=input.endDate;
+     reqBody.budgetOwner=input.budgetOwner;
+     reqBody.budget=input.budget;
+     
+     console.log(reqBody);
+    Router.forwardWithPost(prefix+action+"/name/"+input.name+"/path/"+currentPath+"/folder/"+flag,true,reqBody,function(data){
         callBack(data);
     });
 }
